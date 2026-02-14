23815309b1ea2ca1df4063bee401ab5672e8a1f2
weizis <nastyamik2019@mail.ru>
2026-02-15 01:18:19 +0300

    Update README and add PersonAgeComparer

---
+++ Comparers/PersonAgeComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using DataAlgorithms.Models;
+
+namespace DataAlgorithms.Comparers
+{
+    public class PersonAgeComparer : IComparer<Person>
+    {
+        public int Compare(Person? x, Person? y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+
+            return x.Age.CompareTo(y.Age); 
+        }
+    }
+}
